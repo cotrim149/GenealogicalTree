cd10fa3447bb98f01070322bbe6e1af54e6df670
Victor Cotrim <cotrim149@gmail.com>
2016-05-01 18:04:40 -0300

    Adding brother and sister rules

--- main.pl
+++ main.pl
@@ -1,7 +1,26 @@
 woman(eliane).
+woman(fernanda).
+
 man(albino).
-progenitor(eliane,cotrim).
+man(cotrim).
+man(nicolas).
+man(gustavo).
+
 progenitor(albino,cotrim).
+progenitor(albino,fernanda).
+
+progenitor(eliane,cotrim).
+progenitor(eliane,nicolas).
+progenitor(eliane,gustavo).
+
 
 mother(Mother,Son):- woman(Mother),progenitor(Mother,Son).
 father(Father,Son):- man(Father),progenitor(Father,Son).
+
+brother(Man1,Man2):- man(Man1), man(Man2),
+  progenitor(Progenitor,Man1) , progenitor(Progenitor,Man2),
+  (man(Progenitor); woman(Progenitor)).
+
+sister(Woman,Man):- woman(Woman), man(Man),
+  progenitor(Progenitor,Woman) , progenitor(Progenitor,Man),
+  (man(Progenitor); woman(Progenitor)).
