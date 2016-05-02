578d1ae2990db7d702a95594d7cf8386ff670911
Victor Cotrim <cotrim149@gmail.com>
2016-05-02 13:53:35 -0300

    Adding integration of main.pl in server.pl and refactor header for HTML

--- server.pl
+++ server.pl
@@ -1,5 +1,6 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
+:- consult('main.pl').
 
 server(Port) :- http_server(http_dispatch, [port(Port)]).
 stopServer(Port) :- http_stop_server(Port, []).
@@ -14,16 +15,26 @@ stopServer(Port) :- http_stop_server(Port, []).
 % route localhost:port/foo/bar
 :- http_handler(root('foo/bar'), say_foo_bar(cotrim), []).
 
+% route localhost:port/tree
+:- http_handler(root(tree), genealogicalTree, []).
+
+header(ContentType) :-
+  format('Content-type: ~w~n~n',ContentType).
 % handler for every route
 say_hi(_Request) :-
-  format('Content-type: text/plain~n~n'),
+  header(text/plain),
   format('Hello World!~n').
 
 say_bye(_Request) :-
-  format('Content-type: text/plain~n~n'),
+  header(text/plain),
   format('Bye Bye!~n').
 
 say_foo_bar(Name,_Request) :-
-  format('Content-type: text/plain~n~n'),
-  format('Foo Bar!~n'),
+  header(text/plain),
+  format('Foo Bar ~w!~n',[Name]),
   format(Name).
+
+genealogicalTree(_Request) :-
+  header(text/plain),
+  mother(Mother,cotrim),
+  format(Mother).
