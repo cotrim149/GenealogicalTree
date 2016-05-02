78749458d02487ad6ccdbca227d7541dda57048e
Victor Cotrim <cotrim149@gmail.com>
2016-05-02 11:32:55 -0300

    Adding some exemples for routes #1

--- server.pl
+++ server.pl
@@ -5,9 +5,25 @@ server(Port) :- http_server(http_dispatch, [port(Port)]).
 stopServer(Port) :- http_stop_server(Port, []).
 
 % make routes
-:- http_handler(/, say_hi, []).
+% route localhost:port/
+:- http_handler(root(.), say_hi, []).
+
+% route localhost:port/bye
+:- http_handler(root(bye), say_bye, []).
+
+% route localhost:port/foo/bar
+:- http_handler(root('foo/bar'), say_foo_bar(cotrim), []).
 
 % handler for every route
 say_hi(_Request) :-
-        format('Content-type: text/plain~n~n'),
-        format('Hello World!~n').
+  format('Content-type: text/plain~n~n'),
+  format('Hello World!~n').
+
+say_bye(_Request) :-
+  format('Content-type: text/plain~n~n'),
+  format('Bye Bye!~n').
+
+say_foo_bar(Name,_Request) :-
+  format('Content-type: text/plain~n~n'),
+  format('Foo Bar!~n'),
+  format(Name).
