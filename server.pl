d5dc5a2e19186091c39194167e120ab61da60326
Victor Cotrim <cotrim149@gmail.com>
2016-05-05 12:02:15 -0300

    Adding json exemple

--- server.pl
+++ server.pl
@@ -9,9 +9,6 @@ stopServer(Port) :- http_stop_server(Port, []).
 % route localhost:port/
 :- http_handler(root(.), say_hi, []).
 
-% route localhost:port/bye
-:- http_handler(root(bye), say_bye, []).
-
 % route localhost:port/foo/bar
 :- http_handler(root('foo/bar'), say_foo_bar(cotrim), []).
 
@@ -25,10 +22,6 @@ say_hi(_Request) :-
   header(text/plain),
   format('Hello World!~n').
 
-say_bye(_Request) :-
-  header(text/plain),
-  format('Bye Bye!~n').
-
 say_foo_bar(Name,_Request) :-
   header(text/plain),
   format('Foo Bar ~w!~n',[Name]),
@@ -38,3 +31,39 @@ genealogicalTree(_Request) :-
   header(text/plain),
   mother(Mother,cotrim),
   format(Mother).
+
+
+% JSON support
+:- use_module(library(http/json)).
+% convert prolog terms to json
+:- use_module(library(http/json_convert)).
+% make HTTP client and server
+:- use_module(library(http/http_json)).
+% enable json use
+:- multifile http_json/1.
+
+% adding mime types
+http_json:json_type('application/x-javascript').
+http_json:json_type('text/javascript').
+http_json:json_type('text/x-javascript').
+http_json:json_type('text/x-json').
+
+% make object type for json use
+:- json_object coord(x:float, y:float).
+:- json_object circle(center:coord/2).
+:- json_object point(x:integer, y:integer) + [type=point].
+
+% route localhost:port/tree
+:- http_handler(root(json_test), jsonHandle, []).
+
+jsonHandle(_Request):-
+  prolog_to_json(circle(coord(3.4, 5.6)), JSON_Object),
+  reply_json(JSON_Object).
+
+% json tests
+test(JSON_Object):-
+  prolog_to_json(circle(coord(3.4, 5.6)), JSON_Object),
+  format(user_output, '~w', JSON_Object).
+
+pointTest(Json):-
+  prolog_to_json(point(3,4),Json).
