1bf544a2040ed5678943a7e338968f80ef54a8c1
Ruyther Costa <ruy_fusion@hotmail.com>
2016-05-07 16:08:19 -0300

    Adding the save dynamic data methods

--- main.pl
+++ main.pl
@@ -50,7 +50,7 @@ uncle(Man,Person):- man(Man),
   father(Father,Person),brother(Man,Father)
 ).
 
-%% Database defining rules
+% Database defining rules
 
 definePerson(Name,Gender):-
   (Gender = man , assert(man(Name)));
@@ -60,3 +60,40 @@ defineMother(Name,Son):-
   (man(Son);woman(Son)),
   assert(woman(Name)),
   assert(progenitor(Name,Son)).
+
+defineFather(Name,Son):-
+  (man(Son);woman(Son)),
+  assert(man(Name)),
+  assert(progenitor(Name,Son)).
+
+defineAunt(Name,Nephew):-
+  (man(Nephew);woman(Nephew)),
+  assert(woman(name)),
+  assert(progenitor()).
+
+% Saving the dynamic database with all predicates
+
+save_data:-
+  tell('genealogicalData.txt'),
+  listing(man),
+  listing(woman),
+  listing(progenitor),
+  told.
+
+% Saving the dynamic database with lists
+
+save_data_as_list:-
+  tell('genealogicalDataLists.txt'),
+  do_savings,
+  told.
+
+do_savings:-
+  ( ( mother(A,B)=..X,mother(A,B) )
+  ),
+  decision(A,B),
+  PRED=..X,
+  write(X),write('.'),nl,
+  fail.
+do_savings.
+
+decision(_,_).
