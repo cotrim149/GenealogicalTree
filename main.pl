0850f81e9814924ff6526137c3ec8832a051d8da
Victor Cotrim <cotrim149@gmail.com>
2016-05-08 18:00:54 -0300

    Adding load database file

--- main.pl
+++ main.pl
@@ -80,6 +80,31 @@ save_data:-
   listing(parent),
   told.
 
+% Load data to a dynamic database
+readFile(File,_):-
+  read(File,Line),
+  (
+    ( % if Head of List splitted are ":" than do nothing
+      format(atom(FormattedLine),"~w",Line),
+      split_string(FormattedLine,' -','',[Head|Tail]),
+      Head = ":"
+    );
+    ( % else load data into assert with line information
+      asserta(Line)
+    )
+  ),
+  nl,
+  (
+    (Line \= end_of_file, readFile(File,Line));
+    (Line = end_of_file)
+  ),!.
+
+load_data:-
+  open('genealogicalData.txt',read,File),
+  readFile(File,_),nl,
+  write('File load with success!'),
+  close(File).
+
 % Saving the dynamic database with lists
 
 save_data_as_list:-
